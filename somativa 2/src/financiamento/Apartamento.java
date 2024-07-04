dfd6b8acbe6c2a205e0742d11e1c704e81ed59c2
thiago <thiagosilvalima784@gmail.com>
2024-07-04 11:06:20 -0300

    Update Apartamento.java

--- somativa 2/src/financiamento/Apartamento.java
+++ somativa 2/src/financiamento/Apartamento.java
@@ -4,12 +4,23 @@ public class Apartamento extends FinanciamentoImovel {
     private final int numVagasGaragem;
     private final int numAndar;
 
+    // Construtor da classe
     public Apartamento(double valorImovel, int prazoAnos, double taxaJurosAnual, int numVagasGaragem, int numAndar) {
         super(valorImovel, prazoAnos, taxaJurosAnual);
         this.numVagasGaragem = numVagasGaragem;
         this.numAndar = numAndar;
     }
 
+    // Métodos getters
+    public int getNumVagasGaragem() {
+        return numVagasGaragem;
+    }
+
+    public int getNumAndar() {
+        return numAndar;
+    }
+
+    // Método para calcular o pagamento mensal do financiamento
     @Override
     public double calcularPagamentoMensal() {
         double prazoMeses = getPrazoAnos() * 12;
@@ -18,6 +29,7 @@ public class Apartamento extends FinanciamentoImovel {
                 (Math.pow(1 + taxaMensal, prazoMeses) - 1);
     }
 
+    // Método para exibir os dados específicos do financiamento
     @Override
     public void exibirDadosFinanciamento() {
         System.out.printf("Valor do Imóvel: R$ %.2f%n", getValorImovel());
