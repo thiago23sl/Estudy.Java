cad44a64023a416a16d192621e3013685684388e
thiago <thiagosilvalima784@gmail.com>
2024-07-04 11:06:22 -0300

    Update Casa.java

--- somativa 2/src/financiamento/Casa.java
+++ somativa 2/src/financiamento/Casa.java
@@ -5,12 +5,23 @@ public class Casa extends FinanciamentoImovel {
     private final double areaConstruida;
     private final double tamanhoTerreno;
 
+    // Construtor da classe
     public Casa(double valorImovel, int prazoAnos, double taxaJurosAnual, double areaConstruida, double tamanhoTerreno) {
         super(valorImovel, prazoAnos, taxaJurosAnual);
         this.areaConstruida = areaConstruida;
         this.tamanhoTerreno = tamanhoTerreno;
     }
 
+    // Métodos getters
+    public double getAreaConstruida() {
+        return areaConstruida;
+    }
+
+    public double getTamanhoTerreno() {
+        return tamanhoTerreno;
+    }
+
+    // Método para calcular o pagamento mensal do financiamento
     @Override
     public double calcularPagamentoMensal() {
         double prazoMeses = getPrazoAnos() * 12;
@@ -19,6 +30,19 @@ public class Casa extends FinanciamentoImovel {
         return pagamentoBase + TAXA_SEGURO;
     }
 
+    // Método para aplicar desconto, lançando exceção se o desconto for maior que os juros
+    public void aplicarDesconto(double desconto) throws DescontoMaiorDoQueJurosException {
+        double taxaMensal = getTaxaJurosAnual() / 12 / 100;
+        double jurosMensal = getValorImovel() * taxaMensal;
+
+        if (desconto > jurosMensal) {
+            throw new DescontoMaiorDoQueJurosException("Desconto maior do que os juros mensais.");
+        }
+
+        System.out.printf("Desconto de R$ %.2f aplicado com sucesso.%n", desconto);
+    }
+
+    // Método para exibir os dados específicos do financiamento
     @Override
     public void exibirDadosFinanciamento() {
         System.out.printf("Valor do Imóvel: R$ %.2f%n", getValorImovel());
