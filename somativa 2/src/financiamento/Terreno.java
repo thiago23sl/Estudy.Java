2074929ca1f5c4d6d8ccfabb9e3ec8c6f08c451a
thiago <thiagosilvalima784@gmail.com>
2024-07-05 14:58:37 -0300

    Update Terreno.java

--- somativa 2/src/financiamento/Terreno.java
+++ somativa 2/src/financiamento/Terreno.java
@@ -4,11 +4,13 @@ public class Terreno extends FinanciamentoImovel {
     private static final double ACRESCIMO_INADIMPLENCIA = 1.02;
     private final String tipoZona;
 
+    // Construtor da classe
     public Terreno(double valorImovel, int prazoAnos, double taxaJurosAnual, String tipoZona) {
         super(valorImovel, prazoAnos, taxaJurosAnual);
         this.tipoZona = tipoZona;
     }
 
+    // Método para calcular o pagamento mensal do financiamento
     @Override
     public double calcularPagamentoMensal() {
         double prazoMeses = getPrazoAnos() * 12;
@@ -17,6 +19,12 @@ public class Terreno extends FinanciamentoImovel {
         return pagamentoBase * ACRESCIMO_INADIMPLENCIA;
     }
 
+    // Métodos getters
+    public String getTipoZona() {
+        return tipoZona;
+    }
+
+    // Método para exibir os dados específicos do financiamento
     @Override
     public void exibirDadosFinanciamento() {
         System.out.printf("Valor do Imóvel: R$ %.2f%n", getValorImovel());
@@ -26,4 +34,5 @@ public class Terreno extends FinanciamentoImovel {
         System.out.printf("Pagamento Mensal: R$ %.2f%n", calcularPagamentoMensal());
         System.out.printf("Total do Pagamento: R$ %.2f%n", calcularTotalPagamento());
     }
+
 }
