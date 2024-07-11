1ce6bcefc07a991f9375a6c61534711605e1c8ab
thiago <thiagosilvalima784@gmail.com>
2024-07-10 22:18:41 -0300

    Update Casa.java

--- somativa 2/src/financiamento/Casa.java
+++ somativa 2/src/financiamento/Casa.java
@@ -1,18 +1,20 @@
 package financiamento;
 
+import java.io.Serial;
+
 public class Casa extends FinanciamentoImovel {
-    private static final double TAXA_SEGURO = 80;
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     private final double areaConstruida;
     private final double tamanhoTerreno;
 
-    // Construtor da classe
     public Casa(double valorImovel, int prazoAnos, double taxaJurosAnual, double areaConstruida, double tamanhoTerreno) {
         super(valorImovel, prazoAnos, taxaJurosAnual);
         this.areaConstruida = areaConstruida;
         this.tamanhoTerreno = tamanhoTerreno;
     }
 
-    // Métodos getters
     public double getAreaConstruida() {
         return areaConstruida;
     }
@@ -21,35 +23,21 @@ public class Casa extends FinanciamentoImovel {
         return tamanhoTerreno;
     }
 
-    // Método para calcular o pagamento mensal do financiamento
     @Override
     public double calcularPagamentoMensal() {
         double prazoMeses = getPrazoAnos() * 12;
         double taxaMensal = getTaxaJurosAnual() / 12 / 100;
-        double pagamentoBase = (getValorImovel() / prazoMeses) * (1 + taxaMensal);
-        return pagamentoBase + TAXA_SEGURO;
+        return (getValorImovel() / prazoMeses) * (1 + taxaMensal);
     }
 
-    // Método para aplicar desconto, lançando exceção se o desconto for maior que os juros
-    public void aplicarDesconto(double desconto) throws DescontoMaiorDoQueJurosException {
-        double taxaMensal = getTaxaJurosAnual() / 12 / 100;
-        double jurosMensal = getValorImovel() * taxaMensal;
-
-        if (desconto > jurosMensal) {
-            throw new DescontoMaiorDoQueJurosException("Desconto maior do que os juros mensais.");
-        }
-
-        System.out.printf("Desconto de R$ %.2f aplicado com sucesso.%n", desconto);
-    }
-
-    // Método para exibir os dados específicos do financiamento
     @Override
     public void exibirDadosFinanciamento() {
+        System.out.printf("Tipo de Imóvel: Casa%n");
         System.out.printf("Valor do Imóvel: R$ %.2f%n", getValorImovel());
         System.out.printf("Prazo do Financiamento: %d anos%n", getPrazoAnos());
         System.out.printf("Taxa de Juros Anual: %.2f%%%n", getTaxaJurosAnual());
-        System.out.printf("Área Construída: %.2f m²%n", areaConstruida);
-        System.out.printf("Tamanho do Terreno: %.2f m²%n", tamanhoTerreno);
+        System.out.printf("Área Construída: %.2f m²%n", getAreaConstruida());
+        System.out.printf("Tamanho do Terreno: %.2f m²%n", getTamanhoTerreno());
         System.out.printf("Pagamento Mensal: R$ %.2f%n", calcularPagamentoMensal());
         System.out.printf("Total do Pagamento: R$ %.2f%n", calcularTotalPagamento());
     }
