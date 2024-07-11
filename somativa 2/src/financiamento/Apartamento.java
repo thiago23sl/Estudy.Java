692ff83768eee724cef540a3ea08a4d50f9741c1
thiago <thiagosilvalima784@gmail.com>
2024-07-10 22:18:38 -0300

    Update Apartamento.java

--- somativa 2/src/financiamento/Apartamento.java
+++ somativa 2/src/financiamento/Apartamento.java
@@ -1,17 +1,20 @@
 package financiamento;
 
+import java.io.Serial;
+
 public class Apartamento extends FinanciamentoImovel {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     private final int numVagasGaragem;
     private final int numAndar;
 
-    // Construtor da classe
     public Apartamento(double valorImovel, int prazoAnos, double taxaJurosAnual, int numVagasGaragem, int numAndar) {
         super(valorImovel, prazoAnos, taxaJurosAnual);
         this.numVagasGaragem = numVagasGaragem;
         this.numAndar = numAndar;
     }
 
-    // Métodos getters
     public int getNumVagasGaragem() {
         return numVagasGaragem;
     }
@@ -20,7 +23,6 @@ public class Apartamento extends FinanciamentoImovel {
         return numAndar;
     }
 
-    // Método para calcular o pagamento mensal do financiamento
     @Override
     public double calcularPagamentoMensal() {
         double prazoMeses = getPrazoAnos() * 12;
@@ -29,14 +31,14 @@ public class Apartamento extends FinanciamentoImovel {
                 (Math.pow(1 + taxaMensal, prazoMeses) - 1);
     }
 
-    // Método para exibir os dados específicos do financiamento
     @Override
     public void exibirDadosFinanciamento() {
+        System.out.printf("Tipo de Imóvel: Apartamento%n");
         System.out.printf("Valor do Imóvel: R$ %.2f%n", getValorImovel());
         System.out.printf("Prazo do Financiamento: %d anos%n", getPrazoAnos());
         System.out.printf("Taxa de Juros Anual: %.2f%%%n", getTaxaJurosAnual());
-        System.out.printf("Número de Vagas na Garagem: %d%n", numVagasGaragem);
-        System.out.printf("Número do Andar: %d%n", numAndar);
+        System.out.printf("Número de Vagas na Garagem: %d%n", getNumVagasGaragem());
+        System.out.printf("Número do Andar: %d%n", getNumAndar());
         System.out.printf("Pagamento Mensal: R$ %.2f%n", calcularPagamentoMensal());
         System.out.printf("Total do Pagamento: R$ %.2f%n", calcularTotalPagamento());
     }
