51bff539702e2739c9a989813ae0c3f6d9a6655d
thiago <155448340+thiago23sl@users.noreply.github.com>
2025-04-09 08:51:30 -0300

    att

--- java/Estudos/src/mini_calculadora.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class mini_calculadora {
-    public static void main(String[] args) {
-    System.out.println("me diga um numero e irei calculalo para você");
-
-    Scanner sc = new Scanner(System.in);
-    }
-}
